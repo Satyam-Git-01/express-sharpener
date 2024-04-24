1c65f0c496f0d8e5e116b51d757680fa243c622d
Satyam Choudhary <Satyamchoudhary0007@gmail.com>
2024-04-24 22:44:14 +0530

    Bug Fixes

--- routes/shop.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-
-router.get("/", (req, res, next) => {
-  res.send("<h2>Hello From Express</h2>");
-});
-
-module.exports= router
