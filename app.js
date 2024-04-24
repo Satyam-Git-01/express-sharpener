ecf96022351a2d7b057d2aadbfa32dbc048bacaf
Satyam Choudhary <Satyamchoudhary0007@gmail.com>
2024-04-24 09:16:39 +0530

    Express Middleware and Filter

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const http = require("http");
 const express = require("express");
 const app = express();
-app.use((request, response, next) => {
-  console.log("First Middleware");
-  next();
-});
-app.use((request, response, next) => {
-  console.log("Second Middleware");
-});
+const bodyParse = require('body-parser')
+
+app.use(bodyParse.urlencoded())
+
+app.use('/add-product',(req,res,next)=>{
+  res.send(`<form action="/product" method="post"> <input name="product" type="text">
+  <input name="size" type="number"><button type="submit">Add Product</button></form>`)
+})
+
+app.use('/product',(req,res,next)=>{
+  console.log(req.body)
+  res.redirect('/')
+})
+
+app.use('/',(req,res,next)=>{
+  res.send('<h2>Hello From EXpress</h2>')
+})
+// app.use((request, response, next) => {
+//   console.log("First Middleware");
+//   next();
+// });
+// app.use((request, response, next) => {
+//   console.log("Second Middleware");
+// });
 const server = http.createServer(app);
 app.listen(3300);
