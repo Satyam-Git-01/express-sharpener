33bb138d7727a60e1755fd468a72322bfb0a069e
Satyam Choudhary <Satyamchoudhary0007@gmail.com>
2024-04-24 09:33:20 +0530

    Routers and Filters

--- app.js
+++ app.js
@@ -3,27 +3,13 @@ const express = require("express");
 const app = express();
 const bodyParse = require('body-parser')
 
+const adminRoute= require('./routes/admin')
+const shopRoute= require('./routes/shop')
+
 app.use(bodyParse.urlencoded())
 
-app.use('/add-product',(req,res,next)=>{
-  res.send(`<form action="/product" method="post"> <input name="product" type="text">
-  <input name="size" type="number"><button type="submit">Add Product</button></form>`)
-})
+app.use(adminRoute)
+app.use(shopRoute)
 
-app.use('/product',(req,res,next)=>{
-  console.log(req.body)
-  res.redirect('/')
-})
-
-app.use('/',(req,res,next)=>{
-  res.send('<h2>Hello From EXpress</h2>')
-})
-// app.use((request, response, next) => {
-//   console.log("First Middleware");
-//   next();
-// });
-// app.use((request, response, next) => {
-//   console.log("Second Middleware");
-// });
 const server = http.createServer(app);
 app.listen(3300);
